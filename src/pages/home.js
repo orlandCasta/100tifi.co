107dee06cfa1889f8916a977e683eba07e0565f0
orlandCasta <orlandos.casta@gmail.com>
2021-01-18 17:20:30 -0600

    ending api consumption, styles are integrated

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,14 +4,14 @@ const Home = async () => {
   const characters = await getData();
   const view = `
     <div class="Characters">
-    ${characters.results.map(characters => `
-      <article class="Character-item">
-        <a href="#/${characters.id}/">
-          <img src="${characters.image}" alt="${characters.name}">
-          <h2>${characters.name}</h2>
-        </a>
-      </article>
-    `).join('')}
+      ${characters.results.map(character => `
+        <article class="Character-item">
+          <a href="#/${character.id}/">
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+          </a>
+        </article>
+      `).join('')}
     </div>
   `;
   return view;
--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,14 +1,14 @@
 const API = 'https://rickandmortyapi.com/api/character/';
 
 const getData = async (id) => {
-    const apiURL = id ? `${API}${id}` : API; //if ternario
-    try {
-        const response = await fetch(apiURL); // realizamos el llamado con fech a la url
-        const data = await response.json();// convertimos la respuesta del servidor a un objeto json que pueda iterarse
-        return data;
-    } catch(error){
-        console.log('Fetch Error', error);
-    }
-}
+  const apiURl = id ? `${API}${id}` : API;
+  try {
+    const response = await fetch(apiURl);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log('Fetch Error', error);
+  };
+};
 
 export default getData;
